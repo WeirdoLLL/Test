a8b3aafaa79f9e28b437e1eb02fce0863a55b344
lidexiang <635373676@qq.com>
2023-06-21 09:36:06 +0800

    b

---
+++ TestCode/src/main/java/Test1.java
@@ -0,0 +1,19 @@
+import javax.xml.ws.Action;
+
+/**
+ * @author Weirdo
+ * 日期2023/6/14 19:28
+ */
+public class Test1 {
+
+
+}
+
+//忽略其中的部分格式以及符号字符缺失问题 给我解答一下 我用PowerMock测试When(Insuranceutil.postRestTemplate(documentDownloadVo,testurl， logMsg:"银销产商机电子保单和批单打印接")).thenReturn(map);模拟的方法返回值在dutyInsuranceServiceImpl.insDocumentDownload(documentDownloadVo)里面是null
+
+//测试commit
+//测试commit2
+//测试commit3重新提交
+
+//a
+//b
